64557a6707f7f256dcca6c030cd321f27830d74f
Hyyy_1 <447542807@qq.com>
2021-03-12 17:34:42 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      3 % 2 === 0 ? <div>3是偶数</div> : <div>3是鸡数</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
