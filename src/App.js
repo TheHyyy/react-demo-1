ff9cae3f3427476ffa52bc922df7ab2f075fcb24
Hyyy_1 <447542807@qq.com>
2021-03-13 12:03:13 +0800

    React组件的两种书写方式，函数和class

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
+import React from 'react'
+
 function App() {
   return (
     <div>
-      3 % 2 === 0 ? <div>3是偶数</div> : <div>3是鸡数</div>
+      爸爸
+      <Son />
+    </div>
+  )
+}
+
+class Son extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      n: 0,
+      m: 100,
+    }
+  }
+  addN() {
+    this.setState({ n: this.state.n + 1 })
+  }
+  subM() {
+    this.setState({ m: this.state.m - 1 })
+  }
+  render() {
+    return (
+      <div className="Son">
+        儿子:n {this.state.n}
+        儿子:m {this.state.m}
+        <button onClick={() => this.addN()}>点我n+1</button>
+        <button onClick={() => this.subM()}>点我m-1</button>
+        <GrandSon />
+      </div>
+    )
+  }
+}
+
+const GrandSon = () => {
+  const [n, setN] = React.useState(0)
+  const [m, setM] = React.useState(100)
+  return (
+    <div className="GrandSon">
+      孙子:n {n}
+      孙子:m {m}
+      <button onClick={() => setN(n + 1)}>点我n+1</button>
+      <button onClick={() => setM(m - 1)}>点我m-1</button>
     </div>
   )
 }
