3f5904f3ffb724bd41a2f2a116415e42e9d7ec43
Hyyy_1 <447542807@qq.com>
2021-03-18 23:10:05 +0800

    自己写一个useState

--- src/App.js
+++ src/App.js
@@ -1,52 +1,30 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+const rootElement = document.getElementById('root')
+
+let _state
+
+function myUseState(initialValue) {
+  _state = _state === undefined ? initialValue : _state
+  function setState(newState) {
+    _state = newState
+    render()
+  }
+  return [_state, setState]
+}
+
+const render = () => ReactDOM.render(<App />, rootElement)
 
 function App() {
+  const [n, setN] = myUseState(0)
   return (
-    <div>
-      爸爸
-      <Son />
+    <div className="App">
+      <p>{n}</p>
+      <p>
+        <button onClick={() => setN(n + 1)}>+1</button>
+      </p>
     </div>
   )
 }
 
-class Son extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      n: 0,
-      m: 100,
-    }
-  }
-  addN() {
-    this.setState({ n: this.state.n + 1 })
-  }
-  subM() {
-    this.setState({ m: this.state.m - 1 })
-  }
-  render() {
-    return (
-      <div className="Son">
-        儿子:n {this.state.n}
-        儿子:m {this.state.m}
-        <button onClick={() => this.addN()}>点我n+1</button>
-        <button onClick={() => this.subM()}>点我m-1</button>
-        <GrandSon />
-      </div>
-    )
-  }
-}
-
-const GrandSon = () => {
-  const [n, setN] = React.useState(0)
-  const [m, setM] = React.useState(100)
-  return (
-    <div className="GrandSon">
-      孙子:n {n}
-      孙子:m {m}
-      <button onClick={() => setN(n + 1)}>点我n+1</button>
-      <button onClick={() => setM(m - 1)}>点我m-1</button>
-    </div>
-  )
-}
-
-export default App
+ReactDOM.render(<App />, rootElement)
